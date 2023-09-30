ace9a2dd850fbadf3e61b470cddc2d01750b2ea8
Spinelk <106118038+Spinelk@users.noreply.github.com>
2023-09-30 20:00:45 -0300

    refactorización a 'app.module.ts'

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,34 +1,47 @@
+// Angular
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { FormsModule } from '@angular/forms';
 
+// Vistas
 import { AppComponent } from './app.component';
 import { HomeComponent } from './pages/home/home.component';
 import { LoginComponent } from './pages/login/login.component';
 import { SignupComponent } from './pages/signup/signup.component';
+
+// Componenentes
+import { LayoutComponent } from './pages/layout/layout.component';
+import { NavbarComponent } from './componentes/navbar/navbar.component';
+import { MenuLateralComponent } from './componentes/menu-lateral/menu-lateral.component';
+import { ListaComunidadesComponent } from './componentes/menu-lateral/lista-comunidades/lista-comunidades.component';
+
+// Environment
 import { environment } from '../environments/environment';
 
+// Firebase
 import { AngularFireModule } from '@angular/fire/compat';
 import { AngularFireAuthModule } from '@angular/fire/compat/auth';
-import { LayoutComponent } from './pages/layout/layout.component';
-import { MenuLateralComponent } from './componentes/menu-lateral/menu-lateral.component';
-import { NavbarComponent } from './componentes/navbar/navbar.component';
-import { ListaComunidadesComponent } from './componentes/menu-lateral/lista-comunidades/lista-comunidades.component';
 
 @NgModule({
   declarations: [
+    // Vistas
     AppComponent,
     HomeComponent,
     LoginComponent,
     SignupComponent,
+
+    // Componenentes
     LayoutComponent,
     MenuLateralComponent,
     NavbarComponent,
     ListaComunidadesComponent
   ],
   imports: [
-    BrowserModule, AppRoutingModule, FormsModule, AngularFireModule.initializeApp(environment.firebaseConfig), AngularFireAuthModule
+    // Angular
+    BrowserModule, AppRoutingModule, FormsModule,
+    // Firebase
+    AngularFireModule.initializeApp(environment.firebaseConfig), AngularFireAuthModule
   ],
   providers: [],
   bootstrap: [AppComponent]
